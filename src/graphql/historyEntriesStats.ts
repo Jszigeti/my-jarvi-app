9bd5c1c327e647eb8a5f9a14f1f4945ac230519f
Jszigeti <j.szigeti1993@gmail.com>
2025-03-11 12:57:15 +0100

    Add GraphQL query for response rates

---
+++ src/graphql/historyEntriesStats.ts
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client";
+
+export const GET_RESPONSE_RATES = gql`
+  query GetResponseRates(
+    $user_id: uuid!
+    $start_date: timestamptz!
+    $end_date: timestamptz!
+  ) {
+    historyentries_stats(
+      where: {
+        user_id: { _eq: $user_id }
+        week: { _gte: $start_date, _lte: $end_date }
+      }
+      order_by: { week: desc }
+    ) {
+      type
+      week
+      total_messages_sent
+      total_replies
+      no_reply
+      response_rate
+    }
+  }
+`;
